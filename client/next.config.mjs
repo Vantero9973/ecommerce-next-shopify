e91e2cca37ff61d359f1992917f1a51efea0f7d1
Alex Van Vleet <vantero.dev@gmail.com>
2024-09-21 20:27:36 +0900

    Ignore eslint during build

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["files.stripe.com", "tailwindui.com"],
   },
